a975785e08b1bd49fa73e18a59c678c3e4f86e33
Aranza Cabrera <33462552+alu0100889680@users.noreply.github.com>
2019-12-03 18:25:33 +0100

    7

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
-app.use(require('connect-history-api-fallback')())
+// app.use(require('connect-history-api-fallback')())
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
